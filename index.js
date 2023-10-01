bbd2537c34bbd972b472dc3ed1679814db40e3bc
jmmascorro <jessemascorro88@gmail.com>
2023-10-01 00:43:19 -0400

    updates for deployment again, hopefully last

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { sequelize } from "./db/index.js";
 
 const app = express();
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000
 
 sequelize.sync()
 .then(result => {
